f2247ed0f1890e548512be69d20bf315b89a5089
Jesse Farmer <jessethebuilder@yahoo.com>
2017-02-20 21:55:10 -0800

    New Objects JS

---
+++ app/assets/javascripts/farm_shed/bootstrap_rails_form.js
@@ -0,0 +1,180 @@
+function BootstrapRailsForm(form_selector){
+  // Recognizes a number of classes. Use on form fields to specify functionality
+  // - .req = required
+  // - .ssn = Social Security Number
+  // - .cash = Cash (US)
+
+  this.form = $(form_selector);
+
+  //--- Getters ---------------------------------------
+  this.get_labels = function(){
+    return this.form.find('label');
+  }
+
+  this.get_text_fields = function(){
+    return this.form.find('input[type="text"]');
+  }
+
+  this.get_phone_fields = function(){
+    return this.form.find('input[type="tel"]');
+  }
+
+  this.get_date_fields = function(){
+    return this.form.find('input[type="date"]');
+  }
+
+  //--- Methods ------------------------------------------
+  this.addBootstrapFormClasses = function(){
+    this.get_labels().addClass('control-label');
+    // refactor each field type as a property above
+    this.form.find('input[type="email"], input[type="number"], select, textarea').addClass('form-control');
+    this.get_date_fields().addClass('form-control');
+    this.get_text_fields().addClass('form-control');
+    this.get_phone_fields().addClass('form-control');
+  }
+
+  this.requireFormFields = function(){
+    var requireds = this.form.find('.req');
+
+    for(var i = 0; i < requireds.length; i++){
+      var req = $(requireds[i]);
+      req.attr('required', true);
+
+      var label = req.closest('.form-group').find('.control-label');
+      label.addClass('req_label');
+    }
+
+  }
+
+  this.autoFormatPhoneFields =  function(){
+    var phones = this.get_phone_fields();
+
+    for(var i = 0; i < phones.length; i++){
+      var p = $(phones[i]);
+      p.keypress(function(e){
+        if(e.keyCode == 9 || e.keyCode == 8){ return; }
+
+        console.log(e.keyCode);
+
+        var key = e.key;
+        if(/\d/.test(key)){
+          // if key is a number
+          var t = $(this);
+          var len = t.val().length;
+
+          if(len == 3 || len == 7){
+            t.val(t.val() + '-');
+          }
+
+          if(len == 12){
+            e.preventDefault();
+          }
+        } else {
+          // if key is not a number, do nothing
+          e.preventDefault();
+        }
+      });
+    }
+  }
+
+  this.autoFormatSSNFields = function(){
+    var ssns = this.form.find('.ssn');
+
+    for(var i = 0; i < ssns.length; i++){
+      var s = $(ssns[i]);
+      s.attr('placeholder', "xx-xxx-xxxx")
+      s.keypress(function(e){
+        if(e.keyCode == 9 || e.keyCode == 8){ return; }
+
+
+        var key = e.key;
+
+        if(/\d/.test(key)){
+          // if key is a number
+          var t = $(this);
+          var len = t.val().length;
+
+          if(len == 3 || len == 6){
+            t.val(t.val() + '-');
+          }
+
+          if(len == 11){
+            e.preventDefault();
+          }
+        } else {
+          // if key is not a number, do nothing
+          e.preventDefault();
+        }
+      });
+    }
+  }
+
+  this.autoFormatZipFields = function(){
+    var zips = this.form.find('.zip');
+
+    for(var i = 0; i < zips.length; i++){
+      var z = $(zips[i]);
+      z.keypress(function(e){
+        if(e.keyCode == 9 || e.keyCode == 8){ return; }
+
+
+        var key = e.key;
+
+        if(/\d/.test(key)){
+          // if key is a number
+          var t = $(this);
+          var len = t.val().length;
+
+          if(len == 5){
+            t.val(t.val() + '-');
+          }
+
+          if(len == 10){
+            e.preventDefault();
+          }
+        } else {
+          // if key is not a number, do nothing
+          e.preventDefault();
+        }
+      });
+    }
+  }
+
+  this.autoFormatCashFields = function(){
+    var cashes = this.form.find('.cash');
+
+    for(var i = 0; i < cashes.length; i++){
+      var cash = $(cashes[i]);
+
+      cash.keypress(function(e){
+        // if tab is pressed, move on. This is for Firefox
+        if(e.keyCode == 9 || e.keyCode == 8){ return; }
+
+        var key = e.key;
+        if(!/[0-9.]/.test(key)){
+          // if not a number of a period (.)
+          e.preventDefault();
+        } else {
+          // if value is not a number and optionally a decimial
+          // and optionally 1 or 2 numbers
+
+          var val = $(this).val() + key;
+          if(val.length > 0 && !/^\d+\.?\d{0,2}$/.test(val)){
+            e.preventDefault();
+          }
+        }
+      });
+    }
+  }
+
+  this.all = function(){
+    // entry point
+    this.addBootstrapFormClasses();
+    this.requireFormFields();
+    this.autoFormatPhoneFields();
+    this.autoFormatSSNFields();
+    this.autoFormatCashFields();
+    this.autoFormatZipFields();
+    // this.formatDateFieldsForRails();
+  }
+}
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,20 +1,20 @@
 class Address < ActiveRecord::Base
   include ActionView::Helpers::TagHelper
-  
+
   belongs_to :has_address, :polymorphic => true
 
   STATE_HASH = {"AL"=>"Alabama", "AK"=>"Alaska", "AZ"=>"Arizona", "AR"=>"Arkansas", "CA"=>"California", "CO"=>"Colorado", "CT"=>"Connecticut", "DC"=>"Washington DC", "DE"=>"Delaware", "FL"=>"Florida", "GA"=>"Georgia", "HI"=>"Hawaii", "ID"=>"Idaho", "IL"=>"Illinois", "IN"=>"Indiana", "IA"=>"Iowa", "KS"=>"Kansas", "KY"=>"Kentucky", "LA"=>"Louisiana", "ME"=>"Maine", "MD"=>"Maryland", "MA"=>"Massachusetts", "MI"=>"Michigan", "MN"=>"Minnesota", "MS"=>"Mississippi", "MO"=>"Missouri", "MT"=>"Montana", "NE"=>"Nebraska", "NV"=>"Nevada", "NH"=>"New Hampshire", "NJ"=>"New Jersey", "NM"=>"New Mexico", "NY"=>"New York", "NC"=>"North Carolina", "ND"=>"North Dakota", "OH"=>"Ohio", "OK"=>"Oklahoma", "OR"=>"Oregon", "PA"=>"Pennsylvania", "RI"=>"Rhode Island", "SC"=>"South Carolina", "SD"=>"South Dakota", "TN"=>"Tennessee", "TX"=>"Texas", "UT"=>"Utah", "VT"=>"Vermont", "VA"=>"Virginia", "WA"=>"Washington", "WV"=>"West Virginia", "WI"=>"Wisconsin", "WY"=>"Wyoming"}
 
   def state_short
     return self.state.upcase if STATE_HASH.has_key?(self.state.upcase)
-    
+
     STATE_HASH.each do |k, v|
       return k if self.state.upcase == v.upcase
     end
-    
-    return self.state     
+
+    return self.state
   end
-  
+
   geocoded_by :to_s
 
   reverse_geocoded_by :lat, :lon do |address, results|
@@ -38,15 +38,15 @@ class Address < ActiveRecord::Base
     # html = content_tag :iframe, src: google_embedded_map_url(api_key, prefix: prefix, zoom: zoom)
     html.html_safe
   end
-  
+
   def google_map_link
     "https://www.google.com/maps/place/#{to_s.gsub(' ', '+')}/@#{lat},#{lon},17z/data=!3m1!4b1!4m2!3m1!1s0x548fbf599dca6039:0xf468b51315c688e5"
   end
-  
+
   # def google_embedded_directions(api_key, prefix: nil, zoom: 16)
-    
+
   # end
-  
+
   def google_embedded_map_url(api_key, prefix: nil, maptype: 'roadmap', zoom: 16)
     url = "https://www.google.com/maps/embed/v1/place?key="
     url += api_key
